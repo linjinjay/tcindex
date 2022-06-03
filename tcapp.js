839e6c8fdc50813e1ac2dac6f972cdad2e2e63ac
linjinjay <32966306+linjinjay@users.noreply.github.com>
2022-06-03 16:21:15 -0700

    Create tcapp.js

---
+++ tcapp.js
@@ -0,0 +1,8 @@
+function testPunch() {
+  var a = "a";
+  var d = new Date();
+  var n = d.toLocaleTimeString() + "<br>";
+  document.getElementById('timePunch').innerHTML += n;
+  
+document.getElementById("monDate").innerHTML = new Date();
+  
