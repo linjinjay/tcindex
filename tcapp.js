bc7912bf1790c42f8786da4d524e621d114a7790
linjinjay <32966306+linjinjay@users.noreply.github.com>
2022-06-18 23:58:50 -0700

    Update tcapp.js

--- tcapp.js
+++ tcapp.js
@@ -5,25 +5,92 @@ function testPunch() {
   document.getElementById('timePunch').innerHTML += n;
 }
   
-function dateMon(){
-  var monDay = new Date();
-  var monMonth = monDay.getMonth() + 1;
-  var monDated = monDay.getDate();
-  document.getElementById("monDate").innerHTML = "Mon " + monMonth + "/" + monDated;
-
-
-}
-dateMon()
-
 function currentTime(){
   document.getElementById('currentTime').innerHTML = new Date();
 }
 currentTime()
 
+function dateMon(){
+  var monDay = new Date();
+  var monMonth = monDay.getMonth() + 1;
+  var dayWeekmon = monDay.getDay();
+  var monWeek = dayWeekmon - 1;
+  var monDated = monDay.getDate();
+  var datedMon = monDated - monWeek;
+  document.getElementById("monDate").innerHTML = "Mon " + monMonth + "/" + datedMon;
+}
+dateMon()
+
 const dateTue = () => {
   var tueDay = new Date();
   var tueMonth = tueDay.getMonth() + 1;
+  var dayWeektue = tueDay.getDay();
+  var tueWeek = dayWeektue - 2;
   var tueDated = tueDay.getDate();
-  document.getElementById('tueDate').innerHTML = "Tue" + tueMonth + "/" + tueDated;
+  var datedTue = tueDated - tueWeek;
+  document.getElementById('tueDate').innerHTML = "Tue" + tueMonth + "/" + datedTue;
 }
 dateTue()
+
+const dateWed = () => {
+  var wedDay = new Date();
+  var wedMonth = wedDay.getMonth() + 1;
+  var dayWeekwed = wedDay.getDay();
+  var wedWeek = dayWeekwed - 3;
+  var wedDated = wedDay.getDate();
+  var datedWed = wedDated - wedWeek;
+  document.getElementById('wedDate').innerHTML = "Wed" + wedMonth + "/" + datedWed;
+}
+dateWed()
+
+const dateThur = () => {
+  var thurDay = new Date();
+  var thurMonth = thurDay.getMonth() + 1;
+  var dayWeekthur = thurDay.getDay();
+  var thurWeek = dayWeekthur - 4;
+  var thurDated = thurDay.getDate();
+  var datedThur = thurDated - thurWeek;
+  document.getElementById('thurDate').innerHTML = "Thur" + thurMonth + "/" + datedThur;
+}
+dateThur()
+
+const dateFri = () => {
+  var friDay = new Date();
+  var friMonth = friDay.getMonth() + 1;
+  var dayWeekfri = friDay.getDay();
+  var friWeek = dayWeekfri - 5;
+  var friDated = friDay.getDate();
+  var datedFri = friDated - friWeek;
+  document.getElementById('friDate').innerHTML = "Fri" + friMonth + "/" + datedFri;
+}
+dateFri()
+
+const dateSat = () => {
+  var satDay = new Date();
+  var satMonth = satDay.getMonth() + 1;
+  var dayWeeksat = satDay.getDay();
+  var satWeek = dayWeeksat - 6;
+  var satDated = satDay.getDate();
+  var datedSat = satDated - satWeek;
+  document.getElementById('satDate').innerHTML = "Sat" + satMonth + "/" + datedSat;
+}
+dateSat()
+
+const dateSun = () => {
+  var sunDay = new Date();
+  var sunMonth = sunDay.getMonth() + 1;
+  var dayWeeksun = sunDay.getDay();
+  var sunWeek = dayWeeksun - 7;
+  var sunDated = sunDay.getDate();
+  var datedSun = sunDated - sunWeek;
+  document.getElementById('sunDate').innerHTML = "Sun" + sunMonth + "/" + datedSun;
+}
+dateSun()
+
+function daysinMonth(month, year){  
+  return new Date(year, month, 0).getDate();  
+}
+var toDay = new Date();
+var thisYear = toDay.getFullYear();
+var thisMonth = toDay.getMonth();
+var daysMonth1 = daysinMonth(thisMonth, thisYear); 
